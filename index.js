c0950210b64e2a425df05281516abb7af28c4ffd
Ryan Lindskog <r.lindskog17@gmail.com>
2017-05-30 23:24:10 -0700

    removed toggle function

--- index.js
+++ index.js
@@ -73,8 +73,4 @@ if (isJavaScript) {
 	})
 }
 
-function toggle(e) {
-	
-}
-
 // let builtInModules = [ 'assert', 'buffer', 'child_process', 'cluster', 'console', 'constants', 'crypto', 'dgram', 'dns', 'domain', 'events', 'fs', 'http', 'https', 'module', 'net', 'os', 'path', 'process', 'punycode', 'querystring', 'readline', 'repl', 'stream', 'string_decoder', 'timers', 'tls', 'tty', 'url', 'util', 'v8', 'vm', 'zlib' ]
